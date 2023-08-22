5dc7446dc7cf2bbdd8fd9cc300b1f7f44126c06d
ED <dikeemmanuel54@gmail.com>
2023-08-22 06:04:19 -0400

    up

--- netlify/functions/CheckOut.js
+++ netlify/functions/CheckOut.js
@@ -13,15 +13,15 @@ export const handler = async (req, res) => {
 
 
   const request = JSON.parse(req.body)
-  const {price} = request
+  const {price, name} = request
 
   const session = await stripe.checkout.sessions.create({
      line_items: [{
       price_data: {
       currency: 'usd',
-      unit_amount: price,
+      unit_amount: price * 100,
       product_data: {
-        name: 'Car Repair',
+        name: name,
         description: 'Your massage deposit free',
         images: ['https://plus.unsplash.com/premium_photo-1681873742740-9a0e9eaa4584?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80'],
       },
