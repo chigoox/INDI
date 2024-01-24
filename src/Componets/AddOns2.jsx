a8797b5a4497b4b848ec89a4b6062428acb3b4b4
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-01-24 16:05:07 -0500

    Update AddOns2.jsx

--- src/Componets/AddOns2.jsx
+++ src/Componets/AddOns2.jsx
@@ -30,7 +30,7 @@ function AddOns2({ item, myPackage, setMyPackage }) {
                         'https://images.unsplash.com/photo-1600334089648-b0d9d3028eb2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80'
                         : item == 'Hot Wax' ?
                             'https://images.unsplash.com/photo-1541972270946-e171618455a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2276&q=80'
-                            : item == 'Shroom Tea' ?
+                            : item == 'sensory elixir' ?
                                 'https://images.unsplash.com/38/QoR8Bv1S2SEqH6UcSJCA_Tea.jpg?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80'
                                 : item == 'Smoking' ?
                                     'https://images.unsplash.com/photo-1555959304-4cfdc80a15d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2342&q=80'
