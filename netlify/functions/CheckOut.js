8a5137c83373428ba6ab812410d7ac0e6f4bcea4
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-04-03 18:52:08 -0400

    update

---
+++ netlify/functions/CheckOut.js
@@ -0,0 +1,64 @@
+import Stripe from 'stripe';
+
+
+
+export async function POST(request) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  let data = await request.json();
+  let { price, name } = data
+  const session = await stripe.checkout.sessions.create({
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        unit_amount: price * 100,
+        product_data: {
+          name: name,
+          description: 'Your massage deposit free',
+          images: ['https://plus.unsplash.com/premium_photo-1681873742740-9a0e9eaa4584?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80'],
+        },
+      },
+      quantity: 1,
+    }],
+    mode: 'payment',
+    success_url: `http://indimassage.com/?successBook=true`,
+    cancel_url: `http://indimassage.com/?canceledBook=true`,
+  });
+  return new Response(session.url);
+}
+
+
+
+
+
+
+/* export const handler = async (req, res) => {
+
+
+  const request = JSON.parse(req.body)
+  const { price, name } = request
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        unit_amount: price * 100,
+        product_data: {
+          name: name,
+          description: 'Your massage deposit free',
+          images: ['https://plus.unsplash.com/premium_photo-1681873742740-9a0e9eaa4584?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80'],
+        },
+      },
+      quantity: 1,
+    }],
+    mode: 'payment',
+    success_url: `http://indimassage.netlify.app/?successBook=true`,
+    cancel_url: `http://indimassage.netlify.app/?canceledBook=true`,
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      url: session.url,
+      session: session,
+    }),
+  }
+} */
