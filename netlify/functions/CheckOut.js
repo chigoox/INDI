91b723e544f788ba277bf67a634aa619f4595315
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-04-03 19:02:55 -0400

    update

--- netlify/functions/CheckOut.js
+++ netlify/functions/CheckOut.js
@@ -1,37 +1,18 @@
-import Stripe from 'stripe';
+const fetch = require('node-fetch');
 
-
-
-export async function POST(request) {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-  let data = await request.json();
-  let { price, name } = data
-  const session = await stripe.checkout.sessions.create({
-    line_items: [{
-      price_data: {
-        currency: 'usd',
-        unit_amount: price * 100,
-        product_data: {
-          name: name,
-          description: 'Your massage deposit free',
-          images: ['https://plus.unsplash.com/premium_photo-1681873742740-9a0e9eaa4584?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80'],
-        },
-      },
-      quantity: 1,
-    }],
-    mode: 'payment',
-    success_url: `http://indimassage.com/?successBook=true`,
-    cancel_url: `http://indimassage.com/?canceledBook=true`,
-  });
-  return new Response(session.url);
-}
+const stripe = require('stripe')(process.env.STRIPE_SECRET_API_KEY, {
+  apiVersion: '2020-08-27',
+  appInfo: { // For sample support and debugging, not required for production:
+    name: "stripe-samples/checkout-one-time-payments",
+    version: "0.0.1",
+    url: "https://github.com/stripe-samples/checkout-one-time-payments"
+  }
+});
 
 
 
 
-
-
-/* export const handler = async (req, res) => {
+export const handler = async (req, res) => {
 
 
   const request = JSON.parse(req.body)
@@ -51,8 +32,8 @@ export async function POST(request) {
       quantity: 1,
     }],
     mode: 'payment',
-    success_url: `http://indimassage.netlify.app/?successBook=true`,
-    cancel_url: `http://indimassage.netlify.app/?canceledBook=true`,
+    success_url: `http://indimassage.com/?successBook=true`,
+    cancel_url: `http://indimassage.com/?canceledBook=true`,
   });
   return {
     statusCode: 200,
@@ -61,4 +42,4 @@ export async function POST(request) {
       session: session,
     }),
   }
-} */
+} 
