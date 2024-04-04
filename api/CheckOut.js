2027f10e7095cf0911b582b359153cc74097abf5
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-04-04 13:52:36 -0400

    update

--- api/CheckOut.js
+++ api/CheckOut.js
@@ -15,7 +15,7 @@ export default async function POST(request) {
         unit_amount: price * 100,
         product_data: {
           name: name,
-          description: 'Your massage deposit free',
+          description: 'Your massage deposit fee',
           images: ['https://plus.unsplash.com/premium_photo-1681873742740-9a0e9eaa4584?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80'],
         },
       },
--- src/Pages/SubPages/Booking.jsx
+++ src/Pages/SubPages/Booking.jsx
@@ -158,6 +158,8 @@ const Bookings = ({ bookingInfo, setBookingInfo }) => {
                 name: bookingInfo?.name,
             })
         }).then(res => {
+            console.log(res)
+            console.log(res.json())
             res.json().then(res => {
                 console.log(res)
                 window.location.href = res.url
