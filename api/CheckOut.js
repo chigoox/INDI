0923bcf95b9cc538dfc921a6bb11b370f97adb5a
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-04-04 12:35:24 -0400

    h

--- api/CheckOut.js
+++ api/CheckOut.js
@@ -4,6 +4,7 @@ import Stripe from 'stripe';
 
 export default async function POST(request) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  console.log(request)
   let data = await request.json();
   let { price, name } = data
   const session = await stripe.checkout.sessions.create({
