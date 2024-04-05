54339e2cf8831402ffdd0d36dc1d75f210fa5e50
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-04-05 14:46:40 -0400

    update

--- app/api/PayConfirm/route.js
+++ app/api/PayConfirm/route.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 
 export async function GET(request) {
-
+    const endpointSecret = process.env.STRIPE_WEBOOK_KEY
     const sig = request.headers['stripe-signature'];
 
     let event;
