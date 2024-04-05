67447ee2bfa2c75f800dad0d1c7864f6eb3a7e41
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-04-05 14:20:12 -0400

    up

--- app/Admin/page.jsx
+++ app/Admin/page.jsx
@@ -1,16 +1,16 @@
 
 import React from 'react'
 import ClientManager from './Componets/ClientManager'
-import { fetchInOrder } from '../MyCodes/Database'
+
 
 async function page() {
 
-    const clientList = await fetchInOrder('Users', 'name')
+
 
 
     return (
         <div>
-            <ClientManager clientList={clientList} />
+            <ClientManager clientList={[]} />
         </div>
     )
 }
