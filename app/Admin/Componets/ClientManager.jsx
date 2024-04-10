ef3b3be4860be198142f2f5be3620db3d5806c44
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-04-09 21:15:22 -0400

    update

--- app/Admin/Componets/ClientManager.jsx
+++ app/Admin/Componets/ClientManager.jsx
@@ -7,24 +7,24 @@ function ClientManager({ clientList }) {
     const [menu, setMenu] = useState('All')
     return (
         <div className=' flex  text-white w-full flex-col' >
-            <div className='bg-white h-10 w-full text-xl font-bold'>
+            <div className='bg-white  h-10 w-full text-xl font-bold'>
                 {['All', 'Active', 'Completed'].map((item) => {
                     return (
-                        <Button key={item} className={`${menu == item ? 'bg-blue-500' : 'bg-black-800'} h-full w-1/3 trans shadow-sm rounded-none shadow-black`} onPress={() => { item == menu ? setMenu('All') : setMenu(item) }}>
+                        <Button key={item} className={`${menu == item ? 'bg-rose-700' : 'bg-black-800'} h-full w-1/3 trans border-none shadow-sm rounded-none shadow-black`} onPress={() => { item == menu ? setMenu('All') : setMenu(item) }}>
                             {item}
                         </Button>
                     )
                 })}
 
             </div>
-            <div className='center-col p-2 lg:px-40 border h-auto bg-white text-sm   md:text-base text-gray-700'>
-                <div className='w-full fixed top-10 shadow-sm font-bold bg-black text-whites  h-10  flex items-center justify-between px-4'>
+            <div className='center-col text-white p-2 lg:px-40 border h-auto bg-white text-sm   md:text-base'>
+                <div className='w-full fixed lg:px-40 top-10 shadow-sm font-bold bg-black text-whites  h-10  flex items-center justify-between px-4'>
                     <h1 className='w-1/4'>Name</h1>
                     <h1 className='w-1/4'>Appointment</h1>
                     <h1 className='w-1/3'>Contact</h1>
                     <h1 className='w-1/4 lg:block hidden'>Status</h1>
                 </div>
-                <div className="top-10 relative center-col">
+                <div className="top-10 relative center-col w-full">
                     {(clientList).map((item) => {
                         return (
                             <div key={item} className='w-full bg-black my-1 overflow-hidden rounded shadow-sm text-left   text-white h-32 lg:h-10  flex items-center justify-between p-4'>
